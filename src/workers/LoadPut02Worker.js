340c33ec29335e1f84bb0972b6d0732eed47a67d
NSRB <nasyarobby@gmail.com>
2022-10-11 00:06:07 +0700

    using dummy worker

--- src/workers/LoadPut02Worker.js
+++ src/workers/LoadPut02Worker.js
@@ -1,25 +1,48 @@
 /* eslint-disable no-console */
 const { LoadPut02Queue } = require('../app/controllers/Queue');
 const db = require('../app/db');
+const { generateFakturPajak } = require('./dev/createFakturPajak');
 const { default: status } = require('./status');
 
 /**
  *
  * @param {import("bull/lib/job")} job
  */
-function processJobLoadPut02(job) {
-  return new Promise((res) => {
-    let x = 10;
-    const fn = setInterval(() => {
-      x += 10;
-      job.progress(x);
-      if (x === 50) {
-        clearInterval(fn);
-        res('OK');
-      }
-      // if (Math.random() * 2 > 1) rej(new Error('Ups, error.'));
-    }, 5000);
-  });
+async function processJobLoadPut02(job) {
+  const { npwp, sptId } = job.data;
+  console.log({ npwp, sptId });
+  const draft = await db('SPT_1107PUT')
+    .where('ID', sptId)
+    .whereNull('DELETED_AT')
+    .whereNull('SUBMITTED_AT')
+    .first();
+  if (!draft) throw new Error('Draft tidak ditemukan');
+  await db('LAMP_PUT02').where('ID_SPT', sptId).delete();
+  const data = generateFakturPajak(npwp, 100, draft.TH_PAJAK, draft.MS_PAJAK);
+  return data.reduce(async (prev, fp, index, array) => {
+    await prev;
+    if (fp.id)
+      return db('LAMP_PUT02').insert({
+        ID_SPT: sptId,
+        ID_FAKTUR: fp.id,
+        NPWP_PEMUNGUT: fp.npwpPemungut,
+        NPWP_REKANAN: fp.npwpRekanan,
+        NAMA_REKANAN: fp.namaRekanan,
+        NOMOR_FAKTUR: fp.nomorFaktur,
+        TANGGAL_FAKTUR: new Date(fp.tanggalFaktur),
+        ID_FAKTUR_DIGANTI: fp.fpDiganti,
+        DPP: fp.dpp,
+        PPN: fp.ppn,
+        PPNBM: fp.ppnbm,
+        MASA_PAJAK: fp.masaPajak,
+        TAHUN_PAJAK: fp.tahunPajak,
+        KODE_JENIS_TRANSAKSI: fp.kdJenisTransaksi,
+        STATUS_FAKTUR: fp.statusFaktur,
+        CREATED_AT: new Date(),
+      });
+    console.log({ fp });
+    return null;
+  }, Promise.resolve());
 }
 
 LoadPut02Queue.process(processJobLoadPut02);
