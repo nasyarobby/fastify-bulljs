6282e2381faa8ac561cc747152e4a1c8f8c447fc
NSRB <nasyarobby@gmail.com>
2022-11-29 13:45:55 +0700

    Hello

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,16 @@
-FROM node:14.19.3-stretch
-RUN apt-get update && apt-get install -y openjdk-8-jdk
-
-RUN yarn global add pm2
-
-
-RUN apt-get install -y libaio1 alien
-RUN wget http://yum.oracle.com/repo/OracleLinux/OL7/oracle/instantclient/x86_64/getPackage/oracle-instantclient19.6-basic-19.6.0.0.0-1.x86_64.rpm
-RUN alien -i --scripts oracle-instantclient*.rpm
-RUN rm -f oracle-instantclient*.rpm  
+FROM oraclelinux:8-slim
+RUN microdnf  -y module enable nodejs:16
+RUN microdnf  -y install oracle-instantclient-release-el8
+RUN microdnf  -y install oracle-instantclient-basic nodejs
+RUN microdnf -y install npm
+RUN npm --location=global install pm2 yarn
 
 WORKDIR /server
 
 COPY src/package*.json ./
 RUN yarn install --production
 COPY src/ /server
+VOLUME /hostpipe
 EXPOSE 3000
 
 RUN export PATH=/usr/lib/oracle/21/client64/bin:$PATH
--- src/workers/LoadPut02Worker.js
+++ src/workers/LoadPut02Worker.js
@@ -2,6 +2,7 @@
 const { spawn } = require('child_process');
 const { LoadPut02Queue } = require('../app/controllers/Queue');
 const db = require('../app/db');
+const executeJarWithPipe = require('./executeJarWithPipe');
 // const { generateFakturPajak } = require('./dev/createFakturPajak');
 const { default: status } = require('./status');
 
@@ -10,56 +11,60 @@ const { default: status } = require('./status');
  * @param {import("bull/lib/job")} job
  */
 function processJobLoadPut02(job) {
-  return new Promise((res, rej) => {
-    const { npwp, sptId } = job.data;
-    const child = spawn(
-      `java`,
-      ['-Duser.timezone=UTC', '-jar', './workers/bin/load-put2-1.3.jar', npwp, sptId, 100],
-      { shell: '/bin/bash' }
-    );
-    let currentError;
-    child.stdout.on('data', (chunk) => {
-      Buffer.from(chunk)
-        .toString()
-        .split('\n')
-        .forEach((row, index) => {
-          try {
-            if (row) job.log(row);
-            // const {
-            //   // status,
-            //   // message,
-            //   // idSpt,
-            //   totalData,
-            //   processed,
-            // } = JSON.parse(row);
-            // const progress = Math.ceil((processed * 100) / totalData);
-            job.progress(index);
-          } catch (err) {
-            if (err.name === 'SyntaxError') {
-              console.log(row);
-            } else {
-              throw err;
+  const { npwp, sptId } = job.data;
+  if (process.env.USE_JAR_INSIDE_CONTAINER) {
+    return new Promise((res, rej) => {
+      const child = spawn(
+        `java`,
+        ['-Duser.timezone=UTC', '-jar', './workers/bin/load-put2-1.3.jar', npwp, sptId, 100],
+        { shell: '/bin/bash' }
+      );
+      let currentError;
+      child.stdout.on('data', (chunk) => {
+        Buffer.from(chunk)
+          .toString()
+          .split('\n')
+          .forEach((row, index) => {
+            try {
+              if (row) job.log(row);
+              // const {
+              //   // status,
+              //   // message,
+              //   // idSpt,
+              //   totalData,
+              //   processed,
+              // } = JSON.parse(row);
+              // const progress = Math.ceil((processed * 100) / totalData);
+              job.progress(index);
+            } catch (err) {
+              if (err.name === 'SyntaxError') {
+                console.log(row);
+              } else {
+                throw err;
+              }
             }
-          }
-        });
-    });
+          });
+      });
 
-    child.stderr.on('data', (chunk) => {
-      console.log(Buffer.from(chunk).toString());
-    });
+      child.stderr.on('data', (chunk) => {
+        console.log(Buffer.from(chunk).toString());
+      });
 
-    child.on('error', (err) => {
-      console.log('ERROR!');
-      console.error(err);
-      currentError = err;
-    });
+      child.on('error', (err) => {
+        console.log('ERROR!');
+        console.error(err);
+        currentError = err;
+      });
 
-    child.on('close', (code) => {
-      console.log('Closed ', code);
-      if (code === 1) rej(currentError);
-      return res('ok');
+      child.on('close', (code) => {
+        console.log('Closed ', code);
+        if (code === 1) rej(currentError);
+        return res('ok');
+      });
     });
-  });
+  }
+
+  return executeJarWithPipe(`${npwp}-${sptId}`, 'ls -alh /data/services/');
 }
 
 LoadPut02Queue.process(processJobLoadPut02);
